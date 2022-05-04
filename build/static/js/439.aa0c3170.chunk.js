aa230d452b342bdd2110ffbc8ee0c67afb40241a
DenisMalv <kissed03@gmail.com>
2022-05-04 22:26:01 +0300

    deploy

---
+++ build/static/js/439.aa0c3170.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework=self.webpackChunkreact_homework||[]).push([[439],{4439:function(e,r,n){n.r(r);var o=n(501),t=n(184);r.default=function(){return(0,t.jsxs)(t.Fragment,{children:[(0,t.jsx)("div",{children:"Page Not Found"}),(0,t.jsx)(o.rU,{to:"/",children:"Go to Homepage"})]})}}}]);
+//# sourceMappingURL=439.aa0c3170.chunk.js.map
